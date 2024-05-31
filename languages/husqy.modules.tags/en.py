e4fcc19a4ac9ae4665dd3b0eb17a836de20322bc
l3keboy <66973798+l3keboy@users.noreply.github.com>
2024-05-31 10:28:06 +0200

    Add translations

---
+++ languages/husqy.modules.tags/en.py
@@ -0,0 +1,6 @@
+# MODULE TAGS
+response_module_tag_send = "I have send the tag content!"
+response_module_tag_sending_failed = (
+    "Something went wrong while trying to send the tag!"
+)
+response_module_tag_sending_failed_does_not_exist = "Oops! That tag does not exist!"
--- languages/husqy.modules.tempchannels/en.py
+++ languages/husqy.modules.tempchannels/en.py
@@ -2,3 +2,53 @@
 module_tempchannel_default_voice_category = "{bot_name} Voice"
 module_tempchannel_default_text_category = "{bot_name} Text"
 module_create_a_temporary_channel = "Create a TempChannel"
+# Interaction responses
+response_tempchannel_edit_block_failed = (
+    "I am sorry! Something went wrong while trying to block the user(s)!"
+)
+response_tempchannel_edit_block_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to block the user(s), the given channel is not a tempchannel!"
+response_tempchannel_edit_block_success = (
+    "I have blocked the user(s) from the temporary channel!"
+)
+response_tempchannel_edit_claim_failed = "I am sorry! Something went wrong while trying to claim ownership of the temporary channel!"
+response_tempchannel_edit_claim_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to claim ownership of the temporary channel, the given channel is not a tempchannel!"
+response_tempchannel_edit_claim_failed_claim_not_possible = "I am sorry! Something went wrong while trying to claim ownership of the temporary channel, this tempchannel already has an owner!"
+response_tempchannel_edit_claim_success = (
+    "You have claimed ownership of the temporary channel!"
+)
+response_tempchannel_edit_name_failed = "I am sorry! Something went wrong while trying to rename the temporary channel to `{name}`!"
+response_tempchannel_edit_name_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to rename the temporary channel to `{name}`, the given channel is not a tempchannel!"
+response_tempchannel_edit_name_success = (
+    "I have changed the name of the temporary channel to `{name}`!"
+)
+response_tempchannel_onlyfor_block_failed = "I am sorry! Something went wrong while trying to limit the temporary channel to the role!"
+response_tempchannel_onlyfor_block_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to limit the temporary channel to the role, the given channel is not a tempchannel!"
+response_tempchannel_onlyfor_block_success = (
+    "I have limited the the temporary channel to the role!"
+)
+response_tempchannel_edit_slowmode_failed_not_a_valid_number = "I am sorry! The given input is not a valid number!"
+response_tempchannel_edit_slowmode_failed = "I am sorry! Something went wrong while trying to set the slowmode delay of the temporary channel to `{slowmode} seconds`!"
+response_tempchannel_edit_slowmode_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to set the slowmode delay of the temporary channel to `{slowmode} seconds`, the given channel is not a tempchannel!"
+response_tempchannel_edit_slowmode_success = (
+    "I have changed the slowmode delay of the temporary channel to `{slowmode} seconds`!"
+)
+response_tempchannel_edit_slowmode_off_success = "I have turned off slowmode!"
+response_tempchannel_edit_transfer_failed_not_a_valid_member = (
+    "I am sorry! The inserted ID doesn't look like a valid server member!"
+)
+response_tempchannel_edit_transfer_failed = "I am sorry! Something went wrong while trying to transfer the ownership!"
+response_tempchannel_edit_transfer_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to transfer the ownership, the given channel is not a tempchannel!"
+response_tempchannel_edit_transfer_success = "I have transferred ownership to {member}!"
+response_tempchannel_edit_unblock_failed = "I am sorry! Something went wrong while trying to unblock the user(s)!"
+response_tempchannel_edit_unblock_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to unblock the user(s), the given channel is not a tempchannel!"
+response_tempchannel_edit_unblock_success = "I have unblocked the user(s) from the temporary channel!"
+response_tempchannel_edit_user_limit_failed_not_a_valid_number = "I am sorry! The given input is not a valid number!"
+response_tempchannel_edit_user_limit_failed_not_a_valid_range = (
+    "I am sorry! The target user limit must be between 0 and 99!"
+)
+response_tempchannel_edit_user_limit_failed = "I am sorry! Something went wrong while trying to set the user limit of the temporary channel to `{user_limit} users`!"
+response_tempchannel_edit_user_limit_failed_not_a_tempchannel = "I am sorry! Something went wrong while trying to set the user limit of the temporary channel to `{user_limit} users`, the given channel is not a tempchannel!"
+response_tempchannel_edit_user_limit_success = (
+    "I have changed the user limit of the temporary channel to `{user_limit} users`!"
+)
+response_tempchannel_edit_user_limit_off_success = "I have turned off the user limit!"
