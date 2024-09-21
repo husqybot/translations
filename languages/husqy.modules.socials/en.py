78fa700e37a78643b809c9da1588fcac00cff985
l3keboy <66973798+l3keboy@users.noreply.github.com>
2024-09-21 21:31:56 +0200

    add meme translations to socials

--- languages/husqy.modules.socials/en.py
+++ languages/husqy.modules.socials/en.py
@@ -4,3 +4,6 @@ module_socials_rss_new_entry = "Hey @everyone! There is a new entry on {url}, ch
 module_socials_twitch_account_live = "Hey @everyone! **{twitch_account}** is live: {twitch_live_link}"
 module_socials_rss_new_embed_description = "**Title:** {rss_title}\n\n**Description:**\n{rss_description}..."
 unknown = "Unknown"
+meme_footer = "Visit to Reddit to view top new memes!"
+meme_failed = "Something went wrong while getting your meme!"
+meme_delivered = "Here is your random meme!"
