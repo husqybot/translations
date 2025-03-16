530d9c1b009c78c8fc74921690cec503003042a6
l3keboy <66973798+l3keboy@users.noreply.github.com>
2025-03-16 17:33:50 +0100

    Update en.py

--- languages/husqy.modules.invite_tracker/en.py
+++ languages/husqy.modules.invite_tracker/en.py
@@ -1,74 +1,42 @@
-# MODULE RULES
+# MODULE INVITE TRACKER
 removed_user = "(removed_user)"
-accept_rules = "Accept"
-deny_rules = "Deny"
-rules_embed_title = "<server_name> rules"
-response_rules_add_failed_no_language = (
-    "I could not add the rule because the server language is not found!"
+not_in_use = "Not in use!"
+unknown = "Unknown"
+no_timeout_for_user = "Not timed-out"
+no_nickname = "No nickname"
+deafend = "Deafend"
+not_deafend = "Not deafend"
+muted = "Muted"
+not_muted = "Not muted"
+pending = "Pending"
+not_pending = "Not pending"
+unknown_inviter = "Unknown inviter"
+# Responses
+response_invite_create_failed_no_language = (
+    "I was not able to create the invite because the server language is not found!"
 )
-response_rules_add_failed_no_auto_delete = (
-    "I could not add the rule because the servers auto delete is not found!"
+response_invite_create_failed_no_auto_delete = (
+    "I was not able to create the invite because the server auto delete is not found!"
 )
-response_rules_add_failed_no_timezone = (
-    "I could not add the rule because the servers timezone is not found!"
+response_invite_create_failed_no_timezone = (
+    "I was not able to create the invite because the server timezone is not found!"
 )
-response_rules_add_failed_unknown_module_status = (
-    "I could not add the rule because the status of the rules module is unknown!"
+response_invite_create_failed_invalid_max_uses = (
+    "The max uses for an invite link must be between 0 (for infinite) and 100!"
 )
-response_rules_add_failed_disabled = (
-    "I could not add the rule because the rules module is disabled!"
+response_invite_create_failed_invalid_max_age = "The max age for an invite link must be higher than 0!"
+response_invite_create_failed_not_enough_permissions = "You do not have the right permissions to create the invite!"
+response_invite_create_failed = "Something went wrong while trying to create the invite!"
+response_invite_created = "The invite has been created. Invite code: `{code}`. URL: `{url}`."
+response_invite_delete_failed_no_language = (
+    "I was not able to delete the invite because the server language is not found!"
 )
-response_rules_add_failed_limit_reached = (
-    "I could not add the rule because the rule limit has already been reached!"
+response_invite_delete_failed_no_auto_delete = (
+    "I was not able to delete the invite because the server auto delete is not found!"
 )
-response_rules_add_failed_failed_not_administrator = (
-    "I could not add the rule because you are not an administrator!"
+response_invite_delete_failed_no_timezone = (
+    "I was not able to delete the invite because the server timezone is not found!"
 )
-response_rules_add_failed_failed = "Something went wrong while trying to add the rule!"
-response_rules_added = "I have added the rule!"
-response_rules_delete_failed_no_language = (
-    "I could not delete the rule because the server language is not found!"
-)
-response_rules_delete_failed_no_auto_delete = (
-    "I could not delete the rule because the servers auto delete is not found!"
-)
-response_rules_delete_failed_no_timezone = (
-    "I could not delete the rule because the servers timezone is not found!"
-)
-response_rules_delete_failed_unknown_module_status = (
-    "I could not delete the rule because the status of the rules module is unknown!"
-)
-response_rules_delete_failed_disabled = (
-    "I could not delete the rule because the rules module is disabled!"
-)
-response_rules_delete_failed_no_rule_found = "There is no rule found with that ID!"
-response_rules_delete_failed_not_administrator = (
-    "I could not delete the rule because you are not an administrator!"
-)
-response_rules_delete_failed = "Something went wrong while trying to delete the rule!"
-response_rules_deleted = "I have deleted the rule!"
-response_rules_send_failed_no_language = (
-    "I could not send the rules because the servers language is not found!"
-)
-response_rules_send_failed_no_auto_delete = (
-    "I could not send the rules because the servers auto delete is not found!"
-)
-response_rules_send_failed_no_timezone = (
-    "I could not send the rules because the servers timezone is not found!"
-)
-response_rules_send_failed_not_administrator = (
-    "I could not send the rules because you are not an administrator!"
-)
-response_rules_send_failed_no_rules_found = "There are no rules found for this server!"
-response_rules_send_failed_available_in_a_channel = "The rules channel is configured. Members can you that to re-accept or deny. To retrigger to DM, the rules channel must be configured as 'User DM'!"
-response_rules_send_failed = "Something went wrong while sending the rules!"
-response_rules_send_success = "I have send the rules to the user!"
-response_rules_accept_success = "You have accepted the rules. You have been granted the appropriate roles (configured by server admin)!"
-response_rules_denied_success_with_roles = "You have denied the rules. You have been granted the appropriate roles (configured by server admin)!"
-response_rules_denied_success_with_kick = "You have denied the rules. You have been kicked from the server (configured by server admin)!"
-response_rules_denied_success_no_action = (
-    "You have denied the rules. No action has been taken (configured by server admin)."
-)
-response_rules_interact_failed = (
-    "Something went wrong with accepting or denying the rules!"
-)
+response_invite_delete_failed_not_enough_permissions = "You do not have the right permissions to delete the invite!"
+response_invite_delete_failed = "Something went wrong while deleting the invite!"
+response_invite_deleted = "The invite has been deleted!"
